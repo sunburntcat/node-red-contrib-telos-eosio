6df535a481dd2f970118e13c511315eca6d1975d
Nicolas Lopez <nick_lopez_777@hotmail.com>
2020-05-23 11:12:36 -0500

    Created boilerplate telos transfer node out of lower-case

---
+++ telos-transfer/telos-transfer.js
@@ -0,0 +1,13 @@
+//const { Api, JsonRpc, RpcError } = require('eosjs');
+
+module.exports = function(RED) {
+    function TelosTransferNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("telos-transfer",TelosTransferNode);
+};
