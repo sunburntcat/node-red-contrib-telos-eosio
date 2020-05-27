f0479603c2bd25c0d5f15b39e25c6bafc290190c
Nicolas Lopez <nick_lopez_777@hotmail.com>
2020-05-27 16:06:47 -0500

    Ingests necessary config editor info

--- telos-transfer/telos-transfer.js
+++ telos-transfer/telos-transfer.js
@@ -1,9 +1,25 @@
 const { Api, JsonRpc, RpcError } = require('eosjs');
+var fs = require('fs');
 
 module.exports = function(RED) {
     function TelosTransferNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+
+        //node.blockchain = config.blockchain; // Don't need. Chain ID should be enough
+        node.chainid = config.chainid;
+        if (config.endpoint == "other") {
+            node.endpoint = config.customendpoint;
+        } else {
+            node.endpoint = config.endpoint;
+        }
+        node.privkey = fs.readFileSync(config.privkey, 'utf8');
+
+        // The following is for debugging. NEEDS TO BE REMOVED
+        console.log("Chain ID: " + node.chainid);
+        console.log("Endpoint: " + node.endpoint);
+        console.log("Private key: " + node.privkey);
+
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
