fa5f8a0a172b64eb7bfb51b6ac5d400faedf764f
Nicolas Lopez <sunburntcat@gmail.com>
2020-06-13 15:04:07 -0500

    Update to smart contract files

--- telos-push/contract_code/contract.cpp
+++ telos-push/contract_code/contract.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 using namespace eosio;
 
-ACTION iotdata::updatedata(name nodeid,
-                  , float field1,
+ACTION iotdata::updatedata(name nodeid
+                  , float field1
                   , float field2
                   , float field3
                   , float field4
@@ -20,10 +20,26 @@ ACTION iotdata::updatedata(name nodeid,
   require_auth(get_self());
 
   // Init the _data table
-  data_table _data(get_self(), get_self().value);
+  d_type _data(get_self(), get_first_receiver().value);
 
-  // Add the row to the observation set
-  _data.upsert(get_self(), [&](auto& dat) {
+  auto iterator = _data.find(nodeid.value);
+  if( iterator == _data.end() ) // User is not in the table
+  {
+    // Add the row to the observation set
+    _data.emplace(nodeid, [&](auto& dat) {
+      dat.f1 = field1;
+      dat.f2 = field2;
+      dat.f3 = field3;
+      dat.f4 = field4;
+      dat.f5 = field5;
+      dat.f6 = field6;
+      dat.f7 = field7;
+      dat.f8 = field8;
+      dat.f9 = field9;
+      dat.f10 = field10;
+    });
+  } else { // User is in the table
+    _data.modify(iterator, nodeid, [&](auto& dat) {
       dat.f1 = field1;
       dat.f2 = field2;
       dat.f3 = field3;
@@ -36,29 +52,35 @@ ACTION iotdata::updatedata(name nodeid,
       dat.f10 = field10;
     });
 
+  }
+
 }
 
-ACTION iotdata::updatefields(name nodeid,
-                  , float field1,
-                  , float field2
-                  , float field3
-                  , float field4
-                  , float field5
-                  , float field6
-                  , float field7
-                  , float field8
-                  , float field9
-                  , float field10
+ACTION iotdata::updatefields(name nodeid
+                  , string field1
+                  , string field2
+                  , string field3
+                  , string field4
+                  , string field5
+                  , string field6
+                  , string field7
+                  , string field8
+                  , string field9
+                  , string field10
                   ) {
 
   // Only self can run this Action
   require_auth(get_self());
 
   // Init the _fields table
-  fields_table _fields(get_self(), get_self().value);
+  f_type _fields(get_self(), get_first_receiver().value);
 
   // Add the row to the observation set
-  _fields.upsert(get_self(), [&](auto& dat) {
+  auto iterator = _fields.find(nodeid.value);
+  if( iterator == _fields.end() ) // User is not in the table
+  {
+    // Add the row to the observation set
+    _fields.emplace(nodeid, [&](auto& dat) {
       dat.f1 = field1;
       dat.f2 = field2;
       dat.f3 = field3;
@@ -70,6 +92,21 @@ ACTION iotdata::updatefields(name nodeid,
       dat.f9 = field9;
       dat.f10 = field10;
     });
+  } else { // User is in the table
+    _fields.modify(iterator, nodeid, [&](auto& dat) {
+      dat.f1 = field1;
+      dat.f2 = field2;
+      dat.f3 = field3;
+      dat.f4 = field4;
+      dat.f5 = field5;
+      dat.f6 = field6;
+      dat.f7 = field7;
+      dat.f8 = field8;
+      dat.f9 = field9;
+      dat.f10 = field10;
+    });
+
+  }
 
 }
 
--- telos-push/contract_code/contract.hpp
+++ telos-push/contract_code/contract.hpp
@@ -7,22 +7,8 @@ CONTRACT iotdata : public contract {
   public:
     using contract::contract;
 
-    ACTION updatedata(name nodeid,
-                  , float field1,
-                  , float field2
-                  , float field3
-                  , float field4
-                  , float field5
-                  , float field6
-                  , float field7
-                  , float field8
-                  , float field9
-                  , float field10
-                  );
-
-
-    ACTION updatefields(name nodeid,
-                  , string field1,
+    ACTION updatefields(name nodeid
+                  , string field1
                   , string field2
                   , string field3
                   , string field4
@@ -32,30 +18,27 @@ CONTRACT iotdata : public contract {
                   , string field8
                   , string field9
                   , string field10
-                  );
+      );
+
+    ACTION updatedata(name nodeid
+                  , float field1
+                  , float field2
+                  , float field3
+                  , float field4
+                  , float field5
+                  , float field6
+                  , float field7
+                  , float field8
+                  , float field9
+                  , float field10
+      );
+
+    float get_missing_value() { return missing_value; }
 
   private:
 
-    TABLE data {
-      name nodeid;
-      float f1;
-      float f2;
-      float f3;
-      float f4;
-      float f5;
-      float f6;
-      float f7;
-      float f8;
-      float f9;
-      float f10;
-
-      // Set primary key to nodeid which is an eosio name which is a uint64_t
-      auto  primary_key() const { return nodeid; }
-    };
-    typedef multi_index<name("data"), data> data_table;
-
     TABLE fields {
-      name nodeid;
+      uint64_t nodeid;
       string f1;
       string f2;
       string f3;
@@ -70,7 +53,27 @@ CONTRACT iotdata : public contract {
       // Set primary key to nodeid which is an eosio name which is a uint64_t
       auto  primary_key() const { return nodeid; }
     };
-    typedef multi_index<name("fields"), fields> fields_table;
+
+    TABLE data {
+      uint64_t nodeid;
+      float f1;
+      float f2;
+      float f3;
+      float f4;
+      float f5;
+      float f6;
+      float f7;
+      float f8;
+      float f9;
+      float f10;
+
+      // Set primary key to nodeid which is an eosio name which is a uint64_t
+      auto  primary_key() const { return nodeid; }
+    };
+
+    typedef multi_index<name("fields"), fields> f_type;
+    typedef multi_index<name("data"), data> d_type;
+    //typedef multi_index<name("fields"), fields> fields_table;
 
     float missing_value = -9999;
 
